7c4e5f96cf3cc8414993ec1d9401e4274b12c5e8
mauricelanghinrichs <m.langhinrichs@icloud.com>
2021-10-28 21:29:40 +0200

    tests | workflow tests added

--- tests/test_polyloxpgen.py
+++ tests/test_polyloxpgen.py
@@ -2,40 +2,118 @@
 ### this requires pip install pytest
 ### if installed, run >>> pytest within the /tests directory
 
-from ..polyloxpgen import polylox_pgen
-from ..polylox_merge import *
-from .. import polylox_pgen
+import polyloxpgen.merge
+import polyloxpgen.pgen
 import numpy as np
 import pandas as pd
+import os
 
 def test_single_sample_output_bc1020_ld2017():
-    purged_barcodes_ref = np.loadtxt('bc1020_BarPipe.txt', usecols=0, skiprows=1, delimiter='\t', dtype=str)
-    purged_reads_ref = np.loadtxt('bc1020_BarPipe.txt', usecols=1, skiprows=1, delimiter='\t')
-    data_minrecs_ref = np.loadtxt('bc1020_BarPipe.txt', usecols=3, skiprows=1, delimiter='\t', dtype=int)
-    data_pgen_ref = np.loadtxt('bc1020_BarPipe.txt', usecols=4, skiprows=1, delimiter='\t')
+    ref_file = './original/bc1020_BarPipe.txt'
+    purged_barcodes_ref = np.loadtxt(ref_file, usecols=0, skiprows=1, delimiter='\t', dtype=str)
+    purged_reads_ref = np.loadtxt(ref_file, usecols=1, skiprows=1, delimiter='\t')
+    data_minrecs_ref = np.loadtxt(ref_file, usecols=3, skiprows=1, delimiter='\t', dtype=int)
+    data_pgen_ref = np.loadtxt(ref_file, usecols=4, skiprows=1, delimiter='\t')
 
-    print(purged_barcodes_ref)
-    print(purged_reads_ref)
-    print(data_minrecs_ref)
-    print(data_pgen_ref)
+    # run polylox_merge
+    df_merged = polyloxpgen.merge.polylox_merge(['./original/bc1020.barcode.count.txt'],
+                                    ['bc1020'], './temp/', 'bc1020_merged')
 
-    # TODO: finish test when outer loading part is done
-    assert 2 == 3
+    # run polylox_pgen
+    df_pgen = polyloxpgen.pgen.polylox_pgen('./temp/bc1020_merged.txt',
+                                        './temp/', 'bc1020_pgen',
+                                        path_matrix_type='ld_2017')
+
+    # read back from resulted file (corresponds to df_pgen)
+    df_res = pd.read_csv('./temp/bc1020_pgen.txt', sep='\t', index_col=0)
+
+    # delete temporary files in the end
+    if os.path.isfile('./temp/bc1020_merged.txt'):
+        os.remove('./temp/bc1020_merged.txt')
+    if os.path.isfile('./temp/bc1020_pgen.txt'):
+        os.remove('./temp/bc1020_pgen.txt')
+
+    # check if the same set of barcodes comes out
+    assert set(purged_barcodes_ref)==set(df_res.index.to_numpy(dtype=str))
+
+    # then reindex to get the barcode order the same
+    df_res = df_res.reindex(purged_barcodes_ref)
+
+    # all remaining checks on the reorder df_res
+    assert np.all(purged_barcodes_ref==df_res.index.to_numpy(dtype=str))
+    assert np.all(purged_reads_ref==df_res.bc1020)
+    assert np.all(data_minrecs_ref==df_res.MinRec)
+    assert np.allclose(data_pgen_ref, df_res.Pgen)
 
 def test_single_sample_output_bc1022_ld2017():
-    purged_barcodes_ref = np.loadtxt('bc1022_BarPipe.txt', usecols=0, skiprows=1, delimiter='\t', dtype=str)
-    purged_reads_ref = np.loadtxt('bc1022_BarPipe.txt', usecols=1, skiprows=1, delimiter='\t')
-    data_minrecs_ref = np.loadtxt('bc1022_BarPipe.txt', usecols=3, skiprows=1, delimiter='\t', dtype=int)
-    data_pgen_ref = np.loadtxt('bc1022_BarPipe.txt', usecols=4, skiprows=1, delimiter='\t')
+    purged_barcodes_ref = np.loadtxt('./original/bc1022_BarPipe.txt', usecols=0, skiprows=1, delimiter='\t', dtype=str)
+    purged_reads_ref = np.loadtxt('./original/bc1022_BarPipe.txt', usecols=1, skiprows=1, delimiter='\t')
+    data_minrecs_ref = np.loadtxt('./original/bc1022_BarPipe.txt', usecols=3, skiprows=1, delimiter='\t', dtype=int)
+    data_pgen_ref = np.loadtxt('./original/bc1022_BarPipe.txt', usecols=4, skiprows=1, delimiter='\t')
 
-    print(purged_barcodes_ref)
-    print(purged_reads_ref)
-    print(data_minrecs_ref)
-    print(data_pgen_ref)
+    # run polylox_merge
+    df_merged = polyloxpgen.merge.polylox_merge(['./original/bc1022.barcode.count.txt'],
+                                    ['bc1022'], './temp/', 'bc1022_merged')
 
-    # TODO: finish test when outer loading part is done
-    assert 2 == 3
+    # run polylox_pgen
+    df_pgen = polyloxpgen.pgen.polylox_pgen('./temp/bc1022_merged.txt',
+                                        './temp/', 'bc1022_pgen',
+                                        path_matrix_type='ld_2017')
 
-def test_two_sample_output():
-    # TODO placeholder ...
-    assert 2 == 2
+    # read back from resulted file (corresponds to df_pgen)
+    df_res = pd.read_csv('./temp/bc1022_pgen.txt', sep='\t', index_col=0)
+
+    # delete temporary files in the end
+    if os.path.isfile('./temp/bc1022_merged.txt'):
+        os.remove('./temp/bc1022_merged.txt')
+    if os.path.isfile('./temp/bc1022_pgen.txt'):
+        os.remove('./temp/bc1022_pgen.txt')
+
+    # check if the same set of barcodes comes out
+    assert set(purged_barcodes_ref)==set(df_res.index.to_numpy(dtype=str))
+
+    # then reindex to get the barcode order the same
+    df_res = df_res.reindex(purged_barcodes_ref)
+
+    # all remaining checks on the reorder df_res
+    assert np.all(purged_barcodes_ref==df_res.index.to_numpy(dtype=str))
+    assert np.all(purged_reads_ref==df_res.bc1022)
+    assert np.all(data_minrecs_ref==df_res.MinRec)
+    assert np.allclose(data_pgen_ref, df_res.Pgen)
+
+def test_two_sample_output_bc1020_bc1022_uniform():
+    # load reference dataframes
+    df_merged_ref = pd.read_csv('./original/bc1020_bc1022_merged.txt', sep='\t', index_col=0)
+    df_pgen_ref = pd.read_csv('./original/bc1020_bc1022_pgen_uniform.txt', sep='\t', index_col=0)
+
+    # run polylox_merge
+    df_merged = polyloxpgen.merge.polylox_merge(['./original/bc1020.barcode.count.txt',
+                                                './original/bc1022.barcode.count.txt'],
+                                    ['bc1020', 'bc1022'], './temp/', 'bc1020_bc1022_merged')
+
+    # run polylox_pgen
+    df_pgen = polyloxpgen.pgen.polylox_pgen('./temp/bc1020_bc1022_merged.txt',
+                                        './temp/', 'bc1020_bc1022_pgen',
+                                        path_matrix_type='uniform')
+
+    # read back from resulted files (correspond to df_merged and df_pgen)
+    df_merged_res = pd.read_csv('./temp/bc1020_bc1022_merged.txt', sep='\t', index_col=0)
+    df_pgen_res = pd.read_csv('./temp/bc1020_bc1022_pgen.txt', sep='\t', index_col=0)
+
+    # delete temporary files in the end
+    if os.path.isfile('./temp/bc1020_bc1022_merged.txt'):
+        os.remove('./temp/bc1020_bc1022_merged.txt')
+    if os.path.isfile('./temp/bc1020_bc1022_pgen.txt'):
+        os.remove('./temp/bc1020_bc1022_pgen.txt')
+
+    # compare merge dataframes
+    assert np.all(df_merged_ref.index.to_numpy(dtype=str)==df_merged_res.index.to_numpy(dtype=str))
+    assert np.all(df_merged_ref.bc1020==df_merged_res.bc1020)
+    assert np.all(df_merged_ref.bc1022==df_merged_res.bc1022)
+
+    # compare pgen dataframes
+    assert np.all(df_pgen_ref.index.to_numpy(dtype=str)==df_pgen_res.index.to_numpy(dtype=str))
+    assert np.all(df_pgen_ref.bc1020==df_pgen_res.bc1020)
+    assert np.all(df_pgen_ref.bc1022==df_pgen_res.bc1022)
+    assert np.all(df_pgen_ref.MinRec==df_pgen_res.MinRec)
+    assert np.allclose(df_pgen_ref.Pgen, df_pgen_res.Pgen)
