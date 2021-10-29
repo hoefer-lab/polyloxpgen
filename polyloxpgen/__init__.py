cddf2d857a3bcaab2c8b431731c9a9040307d513
mauricelanghinrichs <m.langhinrichs@icloud.com>
2021-10-29 15:20:14 +0200

    code | clean up

--- polyloxpgen/__init__.py
+++ polyloxpgen/__init__.py
@@ -8,11 +8,5 @@ __author__ = 'Maurice Langhinrichs <m.langhinrichs@icloud.com>'
 __all__ = ['merge', 'pgen']
 
 # to be able to use "import polyloxpgen" and "polyloxpgen.polylox_merge()":
-# NOTE: this does not seem to work currently...
-# but using separate imports works: import polyloxpgen.merge, etc.
 from .merge import polylox_merge
 from .pgen import polylox_pgen
-# import merge
-# import pgen
-# import merge.polylox_merge
-# import pgen.polylox_pgen
