c82f74e6b02664b44715026bbe8038f31aac140e
mauricelanghinrichs <m.langhinrichs@icloud.com>
2021-10-29 15:28:41 +0200

    v0.1.2 | release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
       name='polyloxpgen',
-      version='0.1.1',
+      version='0.1.2',
       description='Barcode purging and pgen (probability of generation) calculation for Polylox data.',
       url='https://github.com/mauricelanghinrichs/polyloxpgen',
       author='Maurice Langhinrichs',
